fc99f735e28ba718ed595f1d10930d1f9ebba349
arseniiarsenii <arseniivelichko2@gmail.com>
2020-11-19 00:05:47 +0300

    каркас бота, зависимости

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import pymongo
+import telebot
 import sys
 
 # получить ключи доступа из окружения
 if len(sys.argv) < 3:
-    print("""Передайте ключи доступа в аргументы команды:\n
+    print("""Передайте ключи доступа в аргументы команды:
     python3 main.py пароль_от_mongodb токен_доступа_telegram_api\n""")
     sys.exit()
 else:
@@ -10,3 +12,26 @@ else:
         "mongodb_password": sys.argv[1],
         "tg_token": sys.argv[2]
     }
+
+# инициализировать бота
+bot = telebot.TeleBot(credentials["tg_token"])
+
+# подключить базу данных
+cluster = pymongo.MongoClient(f"mongodb+srv://icton_bot:{credentials['mongodb_password']}@cluster0.yieez.mongodb.net/Icton?retryWrites=true&w=majority")
+db = cluster['Icton']
+
+
+# получить текст сообщения из БД по его ID
+def msg(db_msg_id, mongo_db=db):
+    messages = mongo_db['messages']
+    return messages.find_one({'_id': db_msg_id})['msg']
+
+# test run
+@bot.message_handler(content_types=['text'])
+def send_message(message, text=msg('1')):
+    bot.send_message(message.chat.id, text)
+
+
+# запустить работу бота в бесконечном цикле
+if __name__ == '__main__':
+    bot.infinity_polling()
